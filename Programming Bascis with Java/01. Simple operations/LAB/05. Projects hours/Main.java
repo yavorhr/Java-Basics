cb784d6bec8db7ccf828d3a8176bcd929544b3f4
Yavor Hristozov <76119513+yavorhr@users.noreply.github.com>
2021-08-18 19:11:02 +0300

    Add files via upload

---
+++ Programming Bascis with Java/01. Simple operations/LAB/05. Projects hours/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        int projects = Integer.parseInt(scanner.nextLine());
+        int time = projects*3;
+
+        System.out.printf("The architect %s will need %d hours to complete %d project/s.",name, time, projects);
+    }
+}
