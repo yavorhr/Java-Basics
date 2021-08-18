7991a9263a4f3555506de5927b9016983db11e3e
Yavor Hristozov <76119513+yavorhr@users.noreply.github.com>
2021-08-18 19:00:07 +0300

    Greeting by name

---
+++ Programming Bascis with Java/01. Simple operations/LAB/03. Greeting by name/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        String output = "Hello, " + name + "!";
+        System.out.println(output);
+    }
+}
